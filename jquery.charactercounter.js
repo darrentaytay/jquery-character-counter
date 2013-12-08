8449f44cfc8d44fe4bc4081887206d52998ba9af
Victor Queiroz <victorqueiroz@outlook.com.br>
2013-12-08 05:57:42 -0200

    Update jquery.charactercounter.js

--- jquery.charactercounter.js
+++ jquery.charactercounter.js
@@ -24,17 +24,37 @@
             counterFormat: '%1',
             counterExceededCssClass: 'exceeded',
             onExceed: function(count) {},
-            onDeceed: function(count) {}
+            onDeceed: function(count) {},
+
+            customFields: ['isjustatestchangeit=thevalueisthis', 'youhaveto=separatethisthatway', 'onlythefirstequalcharacter=willbecatch'],
         }; 
             
         var options = $.extend(defaults, options);
 
         return this.each(function() {
-            $(this).after('<'+ options.counterWrapper +' class="' + options.counterCssClass + '"></'+ options.counterWrapper +'>');
-
+            $(this).after('<'+ options.counterWrapper +customFields(options.customFields)+' class="' + options.counterCssClass + '"></'+ options.counterWrapper +'>');
             bindEvents(this);
             checkCount(this);
         });
+        
+        function customFields (fields)
+        {
+            var i, fields_html;
+      
+            if(fields != null && fields.length>0 && fields != '')
+            {
+                for(i in fields)
+                {
+                split = fields[i].split(/=(.+)?/) /* Split only the first ocurrence of the string '=' */
+                delete split[split.length - 1]
+                field_name = split[0]
+                field_value = split[1]
+                fields_html += ' ' + field_name + '="' + field_value + '"'
+                }
+            }
+            
+            return fields_html
+        }
 
         function renderText(count)
         {
